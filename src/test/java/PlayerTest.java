939446be426eef2c77a9a4323f58ca701184d42a
Michael Vella Zarb <michael.vella.14@um.edu.mt>
2017-04-11 18:18:36 +0200

    added PlayerTest setup

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,5 +1,14 @@
-/**
- * Created by Ella on 11/04/2017.
- */
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class PlayerTest {
+	private Player player;
+	
+	@Before
+    public void setUp() {
+        part1 = new Player();
+    }
 }
