597368fd98dfa4e85ab20b2926696c6d12fd227a
Michael Vella Zarb <michael.vella.14@um.edu.mt>
2017-04-13 13:51:35 +0200

    added Jenkins URL to readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CPS2002Assignment-
+
+Jenkins project: https://jenkins-ict.research.um.edu.mt/job/CPS2002%20-%20EL%20MVZ/
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -6,9 +6,23 @@ import static org.junit.Assert.*;
 
 public class PlayerTest {
 	private Player player;
+	int boardSize;
+	Position position;
+	
 	
 	@Before
     public void setUp() {
         player = new Player();
+        boardSize = 5;    
     }
+	
+	@Test
+	public void setPositionTestNotOutOfBounds() {
+		position = new Position(0, 0);
+		
+		assertTrue(player.setPosition(position));
+		assertFalse(player.setPosition(new Position(6, 6)));
+	}
+	
+	
 }
