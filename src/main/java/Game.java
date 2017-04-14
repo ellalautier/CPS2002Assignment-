375a92862cf6450d33710f67aa0e0613069155e5
Michael Vella Zarb <michael.vella.14@um.edu.mt>
2017-04-14 16:21:02 +0200

    setNumPlayers( ) returns true if number is in valid range

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,13 +3,17 @@ public class Game {
     Player[] players;
     Map map;
 
+    boolean setNumPlayers(int n) {
+        if (n >= 2 && n <= 8) {
+            return true;
+        }
+
+        return false;
+    }
+
     public static void main(String[] args){
         Map m = new Map();
         m.size = 30;
         m.generate();
-    }
-
-    boolean setNumPlayers(int n) {
-        return true;
     }
 }
