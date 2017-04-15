4f9580c96c60eaaec91e9a1cc4ad5c321c387998
Michael Vella Zarb <michael.vella.14@um.edu.mt>
2017-04-15 13:11:01 +0200

    added method stubs to Player.java and basic documentation

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,18 @@
 public class Player {
+    /**
+     * Moves the player by one tile in the direction specified.
+     * @param direction Direction to move in - 'U' -> up, 'D, -> down, 'L' -> left, 'R' -> right
+     */
+    void move(char direction) {
+
+    }
+
+    /**
+     * Sets the player's position.
+     * @param p  Position to set.
+     * @return true: setting position was successful, false: not successful - position out of bounds of the map.
+     */
+    boolean setPosition(Position p) {
+        return false;
+    }
 }
