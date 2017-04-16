15f06ea0b9d98241bb6d38c1195ee5438a4aaeb1
Michael Vella Zarb <michael.vella.14@um.edu.mt>
2017-04-16 11:25:08 +0200

    added failing Player constructor tests

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,37 +1,44 @@
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-/*
+
 public class PlayerTest {
+    private Map map;
 	private Player player;
-	int boardSize;
-	Position position;
-	
-	
+
 	@Before
     public void setUp() {
-        player = new Player();
-        boardSize = 5;    
+	    map = new Map();
+	    map.setMapSize(50, 4);
+	    map.generate();
+        player = new Player(map);
     }
-<<<<<<< HEAD
 
+    @After
+    public void tearDown() {
+	    map = null;
+	    player = null;
+    }
 
+    @Test
+    public void playerConstructorTestStartingPositionAddedToDiscoveredPositions() {
+        assertTrue(player.discoveredPositions.contains(player.startPosition));
+    }
 
-=======
-	
-	@Test
+    @Test
+    public void playerConstructorTestStartingPositionEqualsCurrentPosition() {
+        assertEquals(player.startPosition, player.currentPosition);
+    }
+
+	/*@Test
 	public void setPositionTestNotOutOfBounds() {
 		position = new Position(0, 0);
 		
 		assertTrue(player.setPosition(position));
 		assertFalse(player.setPosition(new Position(6, 6)));
-	}
-	
-	
->>>>>>> 4bfda66e1030e4c30c034e4397415ba44dd41f2e
+	}*/
+
 }
-
-
-*/
